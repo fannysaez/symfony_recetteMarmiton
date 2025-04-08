72c043b512113176ce75a7a434f7bf23944bc691
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-04-08 12:30:55 +0200

    Projet Blanc | mise à jour

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController{
-    #[Route('/home', name: 'app_home')]
+    #[Route('', name: 'home_index')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
