8df3af0c2b90a6f67b267022e75ecdcc09794110
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-04-08 15:28:31 +0200

    mise à jour

--- src/Controller/RecetteController.php
+++ src/Controller/RecetteController.php
@@ -5,9 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
+#[Route('/recette')]
 final class RecetteController extends AbstractController{
-    #[Route('/recette', name: 'app_recette')]
+    #[Route('', name: 'app_recette')]
     public function index(): Response
     {
         return $this->render('recette/index.html.twig', [
