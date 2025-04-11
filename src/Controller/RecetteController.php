766577e73fae19faeb054306f0bb5f660fcee85f
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-04-11 08:48:28 +0200

    mise à jour

--- src/Controller/RecetteController.php
+++ src/Controller/RecetteController.php
@@ -17,8 +17,10 @@ final class RecetteController extends AbstractController
     #[Route('/', name: 'recette_index')]
     public function index(EntityManagerInterface $entityManager): Response
     {
+        // Récupérer toutes les recettes
         $recipes = $entityManager->getRepository(Recipe::class)->findAll();
 
+        // Retourner la vue avec les recettes récupérées
         return $this->render('recette/index.html.twig', [
             'recipes' => $recipes,
         ]);
@@ -39,26 +41,41 @@ final class RecetteController extends AbstractController
 
             // Ajouter la date de création
             $recipe->setCreatedAt(new \DateTimeImmutable());
-            $recipe->setUser($this->getUser());  // Associer l'utilisateur connecté à la recette
 
-            // Sauvegarder la recette
+            // Associer l'utilisateur connecté à la recette
+            $recipe->setUser($this->getUser());
+
+            // Sauvegarder la recette en base de données
             $entityManager->persist($recipe);
             $entityManager->flush();
 
+            // Ajouter un message flash de succès
             $this->addFlash('success', 'Votre recette a bien été créée !');
 
+            // Rediriger vers la page de détail de la recette après la création
             return $this->redirectToRoute('recette_show', ['id' => $recipe->getId()]);
         }
 
+        // Renvoyer le formulaire dans la vue
         return $this->render('recette/create.html.twig', [
             'form' => $form->createView(),
         ]);
     }
 
     #[Route('/{id}', name: 'recette_show', requirements: ['id' => '\d+'], methods: ['GET'])]
-    public function show(Recipe $recipe): Response
+    public function show(int $id, EntityManagerInterface $entityManager): Response
     {
+        // Récupérer la recette en fonction de l'ID
+        $recipe = $entityManager->getRepository(Recipe::class)->find($id);
+
+        // Vérifier si la recette existe
+        if (!$recipe) {
+            throw $this->createNotFoundException('Recette non trouvée');
+        }
+
+        // Renvoyer la recette dans la vue
         return $this->render('recette/show.html.twig', [
             'recipe' => $recipe,
         ]);
-    }}
+    }
+}
