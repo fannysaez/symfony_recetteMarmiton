1089ff35b1747ea3d8c2a262bf50bac596544bc3
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-08-29 09:42:49 +0200

    initial commit

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,37 +6,40 @@ use App\Repository\RecipeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[Route('/user')]
+#[IsGranted('ROLE_USER')]
 final class UserController extends AbstractController
 {
-    #[Route('/user/favoris', name: 'user_favorites')]
-    public function favorites(): Response
+
+    #[Route('/', name: 'user')]
+    public function user(RecipeRepository $recipeRepository): Response
     {
         /** @var \App\Entity\User $user */
-        $user = $this->getUser();
-        if (!$user) {
-            throw $this->createAccessDeniedException('Vous devez être connecté.');
-        }
-        $favorites = $user->getFavorites();
-        return $this->render('user/favorites.html.twig', [
-            'favorites' => $favorites,
+        // $user = $this->getUser();
+
+        // if (!$user) {
+        //     throw $this->createAccessDeniedException('Vous devez être connecté.');
+        // }
+
+        // $recipes = $recipeRepository->findBy(['user' => $user]);
+
+        return $this->render('user/user.html.twig', [
+            // 'recipes' => $user->getRecipes(),
+            // 'user' => $user,
         ]);
     }
 
-    #[Route('/user', name: 'user')]
-    public function user(RecipeRepository $recipeRepository): Response
+    #[Route('/favoris', name: 'user_favorites')]
+    public function favorites(): Response
     {
-        $user = $this->getUser();
+        // /** @var \App\Entity\User $user */
+        // $user = $this->getUser();
+        // if (!$user) {
+        //     throw $this->createAccessDeniedException('Vous devez être connecté.');
+        // }
 
-        if (!$user) {
-            throw $this->createAccessDeniedException('Vous devez être connecté.');
-        }
-
-        $recipe = $recipeRepository->findBy(['user' => $user]);
-
-        return $this->render('user/user.html.twig', [
-            'recipes' => $recipe,
-            'user' => $user,
-        ]);
+        return $this->render('user/favorites.html.twig', []);
     }
 }
